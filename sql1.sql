a21f8c184aacd2174859a9a1eb71118232846658
SteiNNx <jorge_irving_15@hotmail.com>
2019-06-05 20:25:38 -0400

    add registros

--- sql1.sql
+++ sql1.sql
@@ -1,5 +1,6 @@
 use clusteredIndex;
 
+--
 create table customers (
 	customerid int not null primary key identity(1,1),
 	customername char(100) not null,
@@ -7,3 +8,15 @@ create table customers (
 	comments char(189) not null
 )
 go
+
+declare @i int = 1 
+while @i<=80000
+begin
+	insert into customers values(
+		'customersname'+ cast(@i as char),
+		'customersaddress'+ cast(@i as char),
+		'comments'+ cast(@i as char)
+	)
+	set @i +=1
+end
+
